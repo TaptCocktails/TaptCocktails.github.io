cfbe62180e8227857724a90e21e64e8521d51d78
andreaslarsen13 <andreas.larsen@zx-ventures.com>
2019-03-12 19:47:21 -0400

    e

---
+++ src/app.js
@@ -0,0 +1,40 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  '1BXZSZ3QVP',
+  '91db0dafaa0a1070a5b73f7bc4c78032'
+);
+
+const search = instantsearch({
+  indexName: 'Retail',
+  searchClient,
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <article>
+          <h1>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h1>
+          <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+
+        </article>
+      `,
+    },
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  })
+);
+
+search.start();
